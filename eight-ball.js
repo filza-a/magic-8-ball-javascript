c4870b7991479bc038c7e5ac892be53e2b2646e2
Filza Ahmad <64220634+filza-a@users.noreply.github.com>
2022-04-23 12:21:58 +0500

    Update eight-ball.js

--- eight-ball.js
+++ eight-ball.js
@@ -1,1 +1,8 @@
+let userName = ''
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!')
 
+let userQuestion = ''
+console.log(userQuestion)
+
+let randomNumber = Math.floor(Math.random() * 8)
+let eightBall = ''
